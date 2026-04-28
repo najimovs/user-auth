ac8142a33ab69b3abd079a884f0221e4111cc529
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2026-04-28 15:33:23 +0500

    Implement signup

---
+++ src/db.js
@@ -0,0 +1,5 @@
+export const db = {
+	users: new Map(),
+	refreshTokens: new Map(),
+	colors: new Set( [ "Red", "Green", "Blue" ] ),
+}
--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import cors from "@fastify/cors"
 import cookie from "@fastify/cookie"
 import JWT from "jsonwebtoken"
 import argon2 from "argon2"
+import { db } from "./db.js"
 
 const PORT = parseInt( process.env.PORT || "3000" )
+const JWT_ACCESS_TOKEN_SECRET = process.env.JWT_ACCESS_TOKEN_SECRET
+const JWT_REFRESH_TOKEN_SECRET = process.env.JWT_REFRESH_TOKEN_SECRET
 
 const fastify = Fastify( { logger: true } )
 
@@ -13,7 +16,40 @@ fastify.register( cookie )
 
 fastify.get( "/", () => ( { status: "OK" } ) )
 
-fastify.post( "/signup", () => ( { status: "signup" } ) )
+fastify.post( "/signup", async ( req, res ) => {
+
+	const { username, password } = req.body
+
+	if ( db.users.has( username ) ) {
+
+		return res.status( 400 ).send( {
+			code: "APP_USERNAME_EXISTS",
+		} )
+	}
+
+	const user = {
+		username,
+		password: await argon2.hash( password ),
+	}
+
+	const payload = {
+		username,
+	}
+
+	db.users.set( username, user )
+
+	const accessToken = JWT.sign( payload, JWT_ACCESS_TOKEN_SECRET, {
+		expiresIn: 60 * 30,
+	} )
+	const refreshToken = JWT.sign( payload, JWT_REFRESH_TOKEN_SECRET, {
+		expiresIn: 60 * 60 * 24 * 7,
+	} )
+
+	res.status( 201 ).send( {
+		accessToken,
+		refreshToken,
+	} )
+} )
 fastify.post( "/login", () => ( { status: "login" } ) )
 fastify.post( "/refresh", () => ( { status: "refresh" } ) )
 fastify.post( "/colors", () => ( { status: "colors" } ) )
