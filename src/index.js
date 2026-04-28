9dceacce2d21d581c79cd150ece8ac48e33d937e
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2026-04-28 15:13:57 +0500

    Setup server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log( "API" )
+import Fastify from "fastify"
+import cors from "@fastify/cors"
+import cookie from "@fastify/cookie"
+import JWT from "jsonwebtoken"
+import argon2 from "argon2"
+
+const PORT = parseInt( process.env.PORT || "3000" )
+
+const fastify = Fastify( { logger: true } )
+
+fastify.register( cors )
+fastify.register( cookie )
+
+fastify.get( "/", () => ( { status: "OK" } ) )
+
+fastify.listen( { port: PORT, host: "0.0.0.0" } )
